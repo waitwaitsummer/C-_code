b1861d574bdcb3089f81d61409d7b4c6db075568
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-17 18:08:29 +0800

    C#sharp倒序输出一个三位数

---
+++ C#sharp倒序输出一个三位数/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int c = int.Parse(Console.ReadLine());
+int d1, d2, d3;
+Console.WriteLine("请输入一个三位数");
+d1 = c / 100;
+d2 = c / 10 % 10;
+d3 = c % 10;
+Console.WriteLine("个位数{0}\n十位数{1}\n百位数{2}", d3, d2, d1);
---
+++ C#不用函数只用格式转换保留两位小数/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("请输入一个浮点数：");
+double a = double.Parse(Console.ReadLine());
+double b = (int)(a * 100 + 0.5) / 100.0;
+Console.WriteLine(b);
