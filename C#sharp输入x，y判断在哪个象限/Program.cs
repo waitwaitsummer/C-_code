2b896966caaf8dc5ccecf952ada6e3a274a884cb
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-18 19:32:31 +0800

    C#输入三个数求最大数

---
+++ C#sharp输入x，y判断在哪个象限/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double x = (double)(Convert.ToDouble(Console.ReadLine()));
+double y = (double)(Convert.ToDouble(Console.ReadLine()));
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("这个坐标位于第一象限");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("这个坐标位于第三象限");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("这个坐标位于第四象限");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("这个坐标位于第二象限");
+}
+else if (x == 0 && y != 0)
+{
+    Console.WriteLine("这个坐标在x轴上");
+}
+else if (x != 0 && y == 0)
+{
+    Console.WriteLine("这个坐标在y轴上");
+}
+else
+{
+    Console.WriteLine("这个坐标是原点");
+}
---
+++ C#输入三个数求最大数/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = (int)(Convert.ToInt32(Console.ReadLine()));
+int b = (int)(Convert.ToInt32(Console.ReadLine()));
+int c = (int)(Convert.ToInt32(Console.ReadLine()));
+int max = 0;
+if (a > b)
+{
+    max = a;
+    //b = a;
+}
+else
+{
+    max= b;
+}
+if (max < c)
+{
+    max = c;
+}
+else
+{
+    max = max;
+}
+Console.WriteLine("最大数是{0}",max);
