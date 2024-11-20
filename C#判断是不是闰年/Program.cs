ab4d958d2b9912e70bf3ef668a837cfc925e6fc5
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-20 19:17:36 +0800

    C#判断是不是闰年

---
+++ C#判断是不是闰年/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("输入一个年份，判断是不是闰年");
+int Year=int.Parse(Console.ReadLine());
+if ((Year % 4 == 0 && Year % 100 != 0) || Year % 400 == 0)
+{
+    Console.WriteLine("是闰年");
+}
+else
+{
+    Console.WriteLine("不是闰年");
+}
