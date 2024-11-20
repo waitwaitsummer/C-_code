9f0826c31b1839ffe3f74fc36649fb4b0050c8f9
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-20 17:07:53 +0800

    C#判断是几位数并倒序输出

---
+++ C#判断是几位数并倒序输出/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int e = int.Parse(Console.ReadLine());
+int f = 0;
+int g = 0;
+int l = 0;
+Console.WriteLine("请输入一个整数");
+while (e > 0)
+{
+    f = e % 10;
+    g = g * 10 + f;
+    e = e / 10;
+    l++;
+}
+Console.WriteLine("倒序输出是：{0}", g);
+Console.WriteLine("这是一个{0}位数", l);
