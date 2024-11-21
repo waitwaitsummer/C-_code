7b7ebbd02ddb346f9ddb120bd0432712c5acd6e5
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-21 18:38:25 +0800

    C#输入一个非零整数判断是正数还是负数并输出绝对值

---
+++ C#输入一个非零整数判断是正数还是负数并输出绝对值/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("输入一个非0整数");
+int x = int.Parse(Console.ReadLine());
+if (x > 0)
+{
+    Console.Write("是正数，绝对值是");
+    Console.WriteLine(x);
+}
+else if (x == 0)
+{
+    Console.WriteLine("输入数字等于0请重新输入一个非0整数");
+}
+else
+{
+    Console.WriteLine("是负数，绝对值是");
+    Console.WriteLine(-x);
+}
