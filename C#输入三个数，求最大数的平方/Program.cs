72d0fa2f21279dc2cbc3367ce56799c711d1fb06
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-22 19:13:51 +0800

    C#输入三个数，求最大数的平方

---
+++ C#输入三个数，求最大数的平方/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography.X509Certificates;
+Console.WriteLine("请输入三个整数");
+double a=double.Parse(Console.ReadLine());
+double b=double.Parse(Console.ReadLine());
+double c=double.Parse(Console.ReadLine());
+Console.WriteLine("最大数的平方是");
+if (a >= b)
+{
+    if (a >= c)
+    {
+        Console.WriteLine(a * a);
+    }
+}
+else if (b >= c)
+{
+    if (b >= a)
+    {
+        Console.WriteLine(b * b);
+    }
+}
+else if (c >= a)
+{
+    if (c >= b)
+    { Console.WriteLine(c * b); }
+}
