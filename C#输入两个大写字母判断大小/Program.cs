dd3d6ace5f9c2e9e0f290b05562d03aae9482a89
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-23 23:20:46 +0800

    C#输入两个大写字母判断大小

---
+++ C#输入两个大写字母判断大小/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("请输入两个大写字母");
+char one = char.Parse(Console.ReadLine());
+char two = char.Parse(Console.ReadLine());
+if (one > two)
+{
+    Console.WriteLine("{0}>{1}", one, two);
+}
+else if (one < two)
+{
+    Console.WriteLine("{0}<{1}",one, two);
+}
