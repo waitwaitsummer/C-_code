16b57916802e77b8273a444658a98dd3c05cf42d
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-24 00:19:25 +0800

    C++输入圆的半径输出圆的周长

---
+++ C++输入圆的半径输出圆的周长/Project1.cpp
@@ -0,0 +1,27 @@
+﻿// Project1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+/*#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+int main()
+{
+	float r, area;
+	printf("请输入半径：");
+	scanf("%f", &r);
+	area = 3.14 * r * r;
+	printf("该圆的面积是：%f.\n", area);
+	return 0;
+}
+*/
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+
+int main()
+{
+	float r, area;
+	printf("请输入半径：");
+	scanf("%f", &r);
+	area = 3.14 * r * r;
+	printf("该圆的面积是：%f.\n", area);
+	return 0;
+}
