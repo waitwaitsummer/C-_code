fa79449e154fe01756ce0b8991b0e13a4e38a45d
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-25 20:05:09 +0800

    C#体测if语句嵌套

---
+++ C#体测if语句嵌套/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+char sex=Convert.ToChar(Console.ReadLine());
+int number=Convert.ToInt32(Console.ReadLine());
+if (sex == 'F')
+{
+    Console.WriteLine("800米长跑");
+    if (number % 2 == 1)
+    {
+        Console.WriteLine("跳绳");
+    }
+    else
+    {
+        Console.WriteLine("跳绳");
+    }
+}
+else
+{
+    Console.WriteLine("1000米长跑");
+    if (number % 2 == 1)
+    {
+        Console.WriteLine("跳远");
+    }
+    else
+    {
+        Console.WriteLine("俯卧撑");
+    }
+}
