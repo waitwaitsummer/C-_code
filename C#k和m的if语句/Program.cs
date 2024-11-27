e98ccc346989bc214a00118032a0385ff145e9fb
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-27 17:49:06 +0800

    C#k和m的if语句

---
+++ C#k和m的if语句/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+double m = Convert.ToDouble(Console.ReadLine());
+int k = Convert.ToInt32(Console.ReadLine());
+if (k == 0)
+{
+    int a = (int)m;
+    Console.WriteLine(a);
+}
+else if (k == 1)
+{
+    double b = ((int)((m + 0.05) * 10)) / 10.0;
+    Console.WriteLine(b);
+}
