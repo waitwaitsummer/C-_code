f3cf601b8480ef62fab52f6a805abe110a7d3de7
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-11-29 22:48:53 +0800

    C#求前n项和

---
+++ C#求前n项和/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+for (int i = 0; i <= n; i++)
+{
+    sum=sum+i;
+}
+Console.WriteLine(sum);
