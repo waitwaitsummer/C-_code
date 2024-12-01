4df2e8ca1e00a0d8264dae71314a6120f6bffbc4
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-01 21:47:09 +0800

    C#sharpswitch语句

---
+++ C#sharpswitch/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num = int.Parse(Console.ReadLine());
+switch (num)
+{
+    case 0:Console.WriteLine("zero");
+        break;
+    case 1:Console.WriteLine("一帆风顺");
+        break;
+    case 2:Console.WriteLine("二龙戏珠");
+        break;
+    case 3:Console.WriteLine("三生三世");
+        break;
+}
