9833ec8f6ad7a1773f3f2c63007f16a2c8ddcd0e
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-02 23:16:19 +0800

    C#switch练习

---
+++ C#switch练习/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sum = int.Parse(Console.ReadLine());
+switch (sum)
+{
+    case 0:
+        Console.WriteLine("0");
+        break;
+    case 1:
+        Console.WriteLine("1");
+        break;
+    case 3:
+        Console.WriteLine("3");
+        break;
+    case 2:
+        Console.WriteLine("2");
+        break;
+}
