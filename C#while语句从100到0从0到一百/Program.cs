6bad7b5aa5484322a86049d85933d01ee68ab72e
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-04 18:25:19 +0800

    C#while语句从100到0从0到一百

---
+++ C#while语句从100到0从0到一百/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 100;
+while (a >= 0)
+{
+    Console.WriteLine(a);
+    a--;
+}
+a++;
+while (a < 100)
+{
+        Console.WriteLine(a);
+        a++;
+}
