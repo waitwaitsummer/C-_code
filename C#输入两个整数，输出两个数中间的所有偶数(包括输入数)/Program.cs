1be5bd1891028984c5ad23e2b635cde2094a4336
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-05 20:55:11 +0800

    C#输入两个整数，输出两个数中间的所有偶数(包括输入数)

---
+++ C#输入两个整数，输出两个数中间的所有偶数(包括输入数)/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+int min = 0;
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else if (a == b)
+{
+    if (b % 2 == 0)
+    {
+        Console.WriteLine("b");
+    }
+    else
+    { Console.WriteLine("没有偶数"); }
+}
+else
+{
+    max = b;
+    min = a;
+}
+if (min % 2 == 1)
+{
+    min++;
+}
+else if (max % 2 == 0)
+{ 
+    max+=2;
+}
+while (min < max)
+{
+        Console.WriteLine(min);
+        min = min + 2;
+    
+}
