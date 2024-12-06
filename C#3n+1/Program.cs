14c0272b8785739cfdc5923fb259ef770e20a3a8
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-06 22:20:33 +0800

    C#3n+1

---
+++ C#3n+1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+while (a != 1)
+{
+    if (a % 2 == 0)
+    {
+        a = 3 * a + 1;
+    }
+    else
+    {
+        a = a / 2;
+    }
+    b++;
+    Console.WriteLine(a);
+}
