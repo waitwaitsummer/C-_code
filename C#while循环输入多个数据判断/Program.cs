9a2f1cce6f52065817208256514e78452b51479c
summer-and-so-on-big-injustice <2829190184@qq.com>
2024-12-11 19:42:52 +0800

    班上有若干名学生，输入学生的个数，然后输入每一个学生的年龄，计算出来平均年龄，保留到小数点后两位，输出平均年龄。

---
+++ C#while循环输入多个数据判断/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+int sum = 0;
+while (b < a + 1)
+{
+    sum = sum + int.Parse(Console.ReadLine());
+    b++;
+}
+double pj=1.0*sum/a;
+pj = ((int)(pj * 100)) / 100.0;
+Console.WriteLine(pj);
